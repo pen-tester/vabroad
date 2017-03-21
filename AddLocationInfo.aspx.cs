e88d071244e7b682be38fbb110b4f3799aa9d244
Andrew Li <andrew.li1987@yandex.com>
2017-03-21 18:49:41 +0900

    add location form

---
+++ AddLocationInfo.aspx.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AddLocationInfo : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DataSet ds_country = BookDBProvider.getDataSet("uspGetCityWithNoLocationList", new List<SqlParameter>());
+
+        int count = ds_country.Tables[0].Rows.Count;
+
+        for(int i=0;i<count; i++)
+        {
+            DataRow row = ds_country.Tables[0].Rows[i];
+            string url = "http://maps.google.com/maps/api/geocode/json?address=" + String.Format("{0}, {1}",row["City"], row["Country"]) + "&sensor=false";
+            WebRequest request = WebRequest.Create(url);
+            using (WebResponse response = request.GetResponse())
+            {
+                using (var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                {
+                    string resp = reader.ReadToEnd();
+                    JObject jobj = JObject.Parse(resp);
+                    string latitude = jobj["geometry"]["location"]["lat"].ToString();
+                    string longtitude = jobj["geometry"]["location"]["lng"].ToString();
+                    List<SqlParameter> param = new List<SqlParameter>();
+                    param.Add(new SqlParameter("@country", row["Country"]));
+                    param.Add(new SqlParameter("@state", row["StateProvince"]));
+                    param.Add(new SqlParameter("@city", row["City"]));
+                    param.Add(new SqlParameter("@lat", latitude));
+                    param.Add(new SqlParameter("@lng", longtitude));
+                    BookDBProvider.getDataSet("uspAddLatLong", param);
+                }
+            }
+        }
+    }
+}
