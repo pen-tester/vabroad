880b511f4ebc1e2739b5fb91c8268af229780921
Andrew Li <andrew.li1987@yandex.com>
2017-04-30 10:26:00 +0900

    some changes

--- Assets/js/propmap.js
+++ Assets/js/propmap.js
@@ -10,7 +10,7 @@ initialize(markers);
 function showeditmap(propid) {
     var left = ($(window).width() / 2) - (800 / 2);
     var top = ($(window).height() / 2) - (600 / 2);
-    popup = window.open("/userowner/editpropmap.aspx?pid=" + propid, "_blank", "width=800, height=600, top=" + top + ", left=" + left);
+    popup = window.open("/userowner/editpropmap.aspx?pid=" + propid, "popup", "width=800, height=600, top=" + top + ", left=" + left);
 }
 
 function initialize(markers) {
