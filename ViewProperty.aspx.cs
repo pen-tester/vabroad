2a57b7f19d87a8f86b9a9ee8051add24ea80c3b8
Andrew Li <andrew.li1987@yandex.com>
2017-05-31 23:03:36 +0900

    mad mimi for view property page

--- ViewProperty.aspx.cs
+++ ViewProperty.aspx.cs
@@ -1203,7 +1203,7 @@ public partial class ViewProperty : CommonPage
                 {
 
                     byte[] response =
-                    client.UploadValues("https://api.madmimi.com/audience_lists/" + Server.UrlEncode("All contacts") + "/add", new NameValueCollection()
+                    client.UploadValues("https://api.madmimi.com/audience_lists/Travelers/add", new NameValueCollection()
                     {
                        { "username", "noreply@vacations-abroad.com" },
                        { "api_key", "9881316569391d3dbfba35b71670b4b2" },
