00aef257ceb4f3ad25106a3c5eca3b65eb8be44e
Andrew Li <andrew.li1987@yandex.com>
2017-07-03 19:11:42 -0700

    we

--- Assets/js/editproperty.js
+++ Assets/js/editproperty.js
@@ -709,6 +709,9 @@ function init_RatePage() {
     $('#hirate').val(prop_info.HiNightRate);
     $('#currency').val(prop_info.MinRateCurrency);
     $('#rates').val(prop_info.Rates);
+    $('#rates').val($('#rates').val().replaceAll("<br />","\r\n"));
     $('#cancel').val(prop_info.CancellationPolicy);
+    $('#cancel').val($('#cancel').val().replaceAll("<br />", "\r\n"));
     $('#deposit').val(prop_info.DepositRequired);
+    $('#deposit').val($('#deposit').val().replaceAll("<br />", "\r\n"));
 }
